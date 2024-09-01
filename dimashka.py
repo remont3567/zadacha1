2122014609219278159f572db9a163a6826788fe
remont3567 <chegotambilo@gmail.com>
2024-09-01 14:23:24 +0800

    Add files via upload

---
+++ dimashka.py
@@ -0,0 +1,27 @@
+# 1 задача
+dz = 12
+time = 1.5
+kyrc = "Python"
+vremya = time / dz
+print("Курс:", kyrc, ",", "всего задач:", dz, ",", "затрачено часов:", time, ",", "среднее время выполнения:", vremya)
+
+
+# 2 задача
+name = "Влад"
+print("Май нейм из", name)
+age = 14
+print("Май олд из", age)
+age = age + 1 # скоро мне 15 типо
+print("Скоро ай эм будет", age)
+is_student = True
+print("Ай из школьник?", is_student)
+
+
+# 3 задача
+my_string = input("Введите строку: ")
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
